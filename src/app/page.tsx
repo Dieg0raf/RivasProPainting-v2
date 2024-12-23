22b9f39ac1a1f9dd97c0838235fa9a49aa7e4cc9
diegoo <rafael2002diego@gmail.com>
2024-12-23 11:36:28 -0800

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import About from "./components/About/About";
+export default function App() {
+  return (
+    <div className="flex max-w-full justify-center">
+      <h1>Testing for deployment</h1>
+      <About />
+    </div>
+  );
+}
