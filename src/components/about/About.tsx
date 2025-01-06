aabd192a390d2471b6f3088a0511cef1570a8677
diegoo <rafael2002diego@gmail.com>
2025-01-05 16:31:50 -0800

    removed import that was not need

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Card } from "@/components/ui/card";
 import { Heart, Star, Shield, Users } from "lucide-react";
 import HeadlineText from "../shared/HeadlineText";
