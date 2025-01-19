22bd79ebac5abe20213d147aa693e6418766456f
diegoo <rafael2002diego@gmail.com>
2025-01-19 15:27:59 -0800

    add api configuration error handling

--- src/app/api/quotes/route.ts
+++ src/app/api/quotes/route.ts
@@ -1,14 +1,26 @@
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
+
+    const API_URL = process.env.API_URL;
+    const API_KEY = process.env.API_SECRET_KEY;
+
+    if (!API_URL || !API_KEY) {
+        console.error('Missing environment variables:', { API_URL, API_KEY });
+        return NextResponse.json(
+            { error: 'Server configuration error' },
+            { status: 500 }
+        );
+    }
+
     try {
         const values = await request.json();
 
-        const response = await fetch(`${process.env.API_URL}`, {
+        const response = await fetch(`${API_URL}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `${process.env.API_SECRET_KEY}`,
+                'Authorization': `${API_KEY}`,
             },
             body: JSON.stringify(values),
         });
